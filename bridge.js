1771d4655780907a627bf76d1b44f593d16ba5cc
a pineapple <90796271+suspicious-pineapple@users.noreply.github.com>
2026-06-01 20:04:36 +0200

    add proper agent

--- bridge.js
+++ bridge.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 let config = JSON.parse(fs.readFileSync("./config.json",{encoding:"utf-8"}));
-const agent = "pineapple_bridge";
+const agent = "TabbyAPI:1";
 
 let userDetails = await (await fetch("https://aihorde.net/api/v2/find_user",{
     headers:{
@@ -10,15 +10,15 @@ let userDetails = await (await fetch("https://aihorde.net/api/v2/find_user",{
     }
 })).json();
 console.log(userDetails);
-let slots = 1;
+let slots = 3;
 let available = true;
 let pollInterval = 2000;
 
 function boostRate(){
-    pollInterval = 500;
+    pollInterval = 600;
     setTimeout(()=>{
         pollInterval=2000;
-    },5000)
+    },4000)
 }
 
 async function loop(){
@@ -26,23 +26,19 @@ async function loop(){
     if(slots > 0){
         (async ()=>{
             slots-=1;
-
             let polled =await popRequest() 
-            console.log(polled.id);
-            
+            console.log(polled);
             if(polled.payload &&polled.id){
                 let generated = await completionsRequest(polled.payload);
-                console.log("generated:",generated.length);
+                console.log("generated:",generated);
                 console.log(await submitRequest(polled.id,generated));
                 boostRate();
             }
             slots++;
         })();
     }
-
     setTimeout(loop,pollInterval);
 }
-
 loop()
 
 
@@ -58,7 +54,6 @@ async function completionsRequest(payload){
         body: JSON.stringify(body)
     });
     let json = await response.json();
-    
     let text = json.choices[0].text;
     return text;
 }
@@ -72,7 +67,7 @@ async function popRequest(){
     config.model_name
   ],
   "bridge_agent": agent,
-  "threads": 1,
+  "threads": slots,
   "require_upfront_kudos": false,
   "amount": 1,
   "extra_slow_worker": false,
@@ -91,17 +86,12 @@ let response = await fetch("https://aihorde.net/api/v2/generate/text/pop",{
     return await response.json();
 }
 async function submitRequest(id,message){
-
     let meta = [];
-
-
     if(message.toLowerCase().includes("csam") || message.toLowerCase().includes("of minors")){
         meta.push({type:"censorship", value:"csam"})
         message = "Please don't do that.";
         console.log("filter hit :( why are people like this");
     }
-
-
     let body = {
         id:id,
         generation:message,
